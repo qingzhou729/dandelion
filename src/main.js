bd8ce6b13b4ef195d6d05ee3a1c2a9131d246b7b
yuxue.yang <yuxue.yang@husor.com>
2019-02-02 10:57:45 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource';
 import router from './router'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
