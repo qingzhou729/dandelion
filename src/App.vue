70b9da084f435a82c29638192663f3e5f5e11e97
yuxue.yang <yuxue.yang@husor.com>
2019-02-06 21:25:07 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,60 @@
 <template>
     <div id="app">
-        <!-- 头部 -->
-        <el-header style="text-align: right; font-size: 12px">
-            <el-dropdown>
-                <i class="el-icon-setting" style="margin-right: 15px"></i>
-                <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>查看</el-dropdown-item>
-                <el-dropdown-item>新增</el-dropdown-item>
-                <el-dropdown-item>删除</el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-            <span>王小虎</span>
-        </el-header>
-        <!-- 内容 -->
-        <div class="content">
-            <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-                <el-menu :default-openeds="['1','2']">
-                    <el-submenu index="1">
-                        <template slot="title">日常管理</template>
-                        <el-menu-item index="1-1">需求管理</el-menu-item>
-                        <el-menu-item index="1-2">项目管理</el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="2">
-                        <template slot="title">发布管理</template>
-                        <el-menu-item index="2-1">待发布</el-menu-item>
-                        <el-menu-item index="2-2">发布队列</el-menu-item>
-                    </el-submenu>
-                </el-menu>
-            </el-aside>
-            <div class="main-content">
-                <router-view/>
-                <!-- <el-footer>Footer</el-footer> -->
+        <!-- <div v-if="!show">
+            <router-view/>
+        </div> -->
+        <!-- <div v-else> -->
+            <!-- 头部 -->
+            <el-header style="text-align: right; font-size: 12px">
+                <el-dropdown>
+                    <i class="el-icon-setting" style="margin-right: 15px"></i>
+                    <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>查看</el-dropdown-item>
+                    <el-dropdown-item>新增</el-dropdown-item>
+                    <el-dropdown-item>删除</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+                <span>王小虎</span>
+            </el-header>
+            <!-- 内容 -->
+            <div class="content">
+                <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+                    <el-menu :default-openeds="['1','2']">
+                        <el-submenu index="1">
+                            <template slot="title">日常管理</template>
+                            <el-menu-item index="1-1">需求管理</el-menu-item>
+                            <el-menu-item index="1-2">项目管理</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="2">
+                            <template slot="title">发布管理</template>
+                            <el-menu-item index="2-1">待发布</el-menu-item>
+                            <el-menu-item index="2-2">发布队列</el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-aside>
+                <div class="main-content">
+                    <router-view/>
+                    <!-- <el-footer>Footer</el-footer> -->
+                </div>
             </div>
         </div>
-    </div>
+    <!-- </div> -->
 </template>
 
 <script>
 export default {
-    name: 'App'
+    name: 'App',
+    data () {
+        return {
+            show: false,
+        }
+    },
+    mounted() {
+        // console.log(this.$route.name);
+        // if (this.$route.name === 'login') {
+        //     this.show = false;
+        // }
+    },
 }
 </script>
 
