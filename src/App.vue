6d5b42bf464a8353393d1335e31b469ae56ed266
yuxue.yang <yuxue.yang@husor.com>
2019-02-20 14:12:57 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
                     <el-dropdown-item>删除</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
-                <span>yuxue.yang test</span>
+                <span> test</span>
             </el-header>
             <!-- 内容 -->
               <!-- <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
