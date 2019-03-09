53cf14d25e27280657cfaba8cb12a6ea4248b70c
yuxue.yang <yuxue.yang@husor.com>
2019-03-09 21:58:43 +0800

    Df

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                     <el-dropdown-item>新增</el-dropdown-item>
                     <el-dropdown-item>删除</el-dropdown-item>
                     </el-dropdown-menu>
-                </el-dropdown>                <span>123xxxxxxxxx</span>
+                </el-dropdown>                <span>456xxxxxxxx</span>
             </el-header>
             <!-- 内容 -->
               <!-- <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
