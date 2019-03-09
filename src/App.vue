58a58380db9c9a70f20a1d409ac4a2ae20da22e8
Yuki Yang <1072131118@qq.com>
2019-03-09 20:46:41 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
                 <el-dropdown>
                     <i class="el-icon-setting" style="margin-right: 15px"></i>
                     <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>查看</el-dropdown-item>
+                    <el-dropdown-item>查看qqqqqqqqqqq</el-dropdown-item>
                     <el-dropdown-item>新增</el-dropdown-item>
                     <el-dropdown-item>删除</el-dropdown-item>
                     </el-dropdown-menu>
